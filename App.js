f2357ee08f1042a6c9559618f2b1104432e414ba
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-02-12 18:05:46 +0300

    entry page & currency calculation by api

--- App.js
+++ App.js
@@ -1,20 +1,161 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, TextInput, Pressable } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
+
+import { dummyData } from './dummy/data';
+
+
 
 export default function App() {
+
+  const [fromPickerOpen, setFromPickerOpen] = useState(false);
+  const [toPickerOpen, setToPickerOpen] = useState(false);
+
+  const [fromPickerValue, setFromPickerValue] = useState(null);
+  const [toPickerValue, setToPickerValue] = useState(null);
+
+  const [pickerItems, setPickerItems] = useState([]);
+
+  const [convertAmount, setConvertAmount] = useState(null);
+
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+
+    const fetchOptions = async () => {
+
+      const options = {
+        method: 'GET',
+        url: 'https://currency-converter18.p.rapidapi.com/api/v1/supportedCurrencies',
+        headers: {
+          'X-RapidAPI-Key': '2c3b4556d2mshca925566ecbe76ap13f707jsn4dc6d2c0f27a',
+          'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+        }
+      };
+
+      try {
+        /*
+        const response = await axios.request(options);
+        const result = response.data.map(i => ({ label: `${i.name} (${i.symbol})`, value: i.symbol }));
+        */
+       const result = dummyData.map(i => ({ label: `${i.name} (${i.symbol})`, value: i.symbol }));
+        setPickerItems(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchOptions();
+
+  }, [])
+
+  useEffect(() => {
+
+    console.log(`From Value: ${fromPickerValue}`)
+    console.log(`To Value: ${toPickerValue}`)
+  }, [fromPickerValue, toPickerValue])
+
+  
+
+  const handleConvertButton = async () => {
+    console.log("calc")
+
+    const options = {
+      method: 'GET',
+      url: 'https://currency-converter18.p.rapidapi.com/api/v1/convert',
+      params: {
+        from: fromPickerValue,
+        to: toPickerValue,
+        amount: convertAmount
+      },
+      headers: {
+        'X-RapidAPI-Key': '2c3b4556d2mshca925566ecbe76ap13f707jsn4dc6d2c0f27a',
+        'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setResult(response.data.result.convertedAmount)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const handleClearButton = () => {
+    setFromPickerValue(null);
+    setToPickerValue(null);
+    setConvertAmount(null);
+    setResult(null);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center">
+
+      <View className="basis-1/4 w-full justify-center items-center">
+        <View>
+          <Text className="text-4xl">Welcome</Text>
+        </View>
+      </View>
+
+      <View className="basis-3/4 w-full items-center gap-4 pt-4 bg-sky-100">
+        <View className="w-80 z-20">
+          <Text>From:</Text>
+          <DropDownPicker
+            placeholder="Select a currency"
+            searchPlaceholder="Search..."
+            searchable={true}
+            open={fromPickerOpen}
+            setOpen={setFromPickerOpen}
+            onOpen={() => setToPickerOpen(false)}
+            value={fromPickerValue}
+            items={pickerItems}
+            setValue={setFromPickerValue}
+            setItems={setPickerItems}
+          />
+        </View>
+        <View className="w-80 z-10">
+          <Text>To:</Text>
+          <DropDownPicker
+            placeholder="Select a currency"
+            searchPlaceholder="Search..."
+            searchable={true}
+            open={toPickerOpen}
+            setOpen={setToPickerOpen}
+            onOpen={() => setFromPickerOpen(false)}
+            value={toPickerValue}
+            items={pickerItems}
+            setValue={setToPickerValue}
+            setItems={setPickerItems}
+          />
+        </View>
+        <View className="w-80">
+          <Text>Amount:</Text>
+          <TextInput
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChangeText={setConvertAmount}
+            value={convertAmount}
+            keyboardType="numeric"
+          />
+        </View>
+        <View className="w-80">
+          <Pressable onPress={handleConvertButton} disabled={!(fromPickerValue && toPickerValue && convertAmount)} className="bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <Text className="text-white text-center">Convert!</Text>
+          </Pressable>
+        </View>
+        <View className="w-80">
+          {result && <Text className="text-5xl text-center mt-2 pb-0">{parseFloat(result.toFixed(3))}</Text>}
+        </View>
+        <View className="w-80">
+          {
+            result && 
+            <Pressable onPress={handleClearButton} className="focus:outline-none bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+              <Text className="text-white text-center">Clear!</Text>
+            </Pressable>
+          }
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ dummy/data.js
@@ -0,0 +1,481 @@
+export const dummyData = [{
+    "name": "United States Dollar",
+    "symbol": "USD"
+}, {
+    "name": "Albania Lek",
+    "symbol": "ALL"
+}, {
+    "name": "Algeria Dinar",
+    "symbol": "DZD"
+}, {
+    "name": "Angola Kwanza",
+    "symbol": "AOA"
+}, {
+    "name": "Argentina Peso",
+    "symbol": "ARS"
+}, {
+    "name": "Armenia Dram",
+    "symbol": "AMD"
+}, {
+    "name": "Aruba Guilder",
+    "symbol": "AWG"
+}, {
+    "name": "Australia Dollar",
+    "symbol": "AUD"
+}, {
+    "name": "Azerbaijan Manat",
+    "symbol": "AZN"
+}, {
+    "name": "Bahamas Dollar",
+    "symbol": "BSD"
+}, {
+    "name": "Bahrain Dinar",
+    "symbol": "BHD"
+}, {
+    "name": "Bangladesh Taka",
+    "symbol": "BDT"
+}, {
+    "name": "Barbados Dollar",
+    "symbol": "BBD"
+}, {
+    "name": "Belarus Ruble",
+    "symbol": "BYN"
+}, {
+    "name": "Belize Dollar",
+    "symbol": "BZD"
+}, {
+    "name": "Bermuda Dollar",
+    "symbol": "BMD"
+}, {
+    "name": "Bhutan Ngultrum",
+    "symbol": "BTN"
+}, {
+    "name": "Bolivia Bolíviano",
+    "symbol": "BOB"
+}, {
+    "name": "Bosnia and Herzegovina Convertible Marka",
+    "symbol": "BAM"
+}, {
+    "name": "Botswana Pula",
+    "symbol": "BWP"
+}, {
+    "name": "Brazil Real",
+    "symbol": "BRL"
+}, {
+    "name": "Brunei Darussalam Dollar",
+    "symbol": "BND"
+}, {
+    "name": "Bulgaria Lev",
+    "symbol": "BGN"
+}, {
+    "name": "Burundi Franc",
+    "symbol": "BIF"
+}, {
+    "name": "Cambodia Riel",
+    "symbol": "KHR"
+}, {
+    "name": "Canada Dollar",
+    "symbol": "CAD"
+}, {
+    "name": "Cape Verde Escudo",
+    "symbol": "CVE"
+}, {
+    "name": "Cayman Islands Dollar",
+    "symbol": "KYD"
+}, {
+    "name": "Chile Peso",
+    "symbol": "CLP"
+}, {
+    "name": "China Yuan Renminbi",
+    "symbol": "CNY"
+}, {
+    "name": "Colombia Peso",
+    "symbol": "COP"
+}, {
+    "name": "Communauté Financière Africaine (BCEAO) Franc",
+    "symbol": "XOF"
+}, {
+    "name": "Communauté Financière Africaine (BEAC) CFA Franc BEAC",
+    "symbol": "XAF"
+}, {
+    "name": "Comorian Franc",
+    "symbol": "KMF"
+}, {
+    "name": "Comptoirs Français du Pacifique (CFP) Franc",
+    "symbol": "XPF"
+}, {
+    "name": "Congo/Kinshasa Franc",
+    "symbol": "CDF"
+}, {
+    "name": "Costa Rica Colon",
+    "symbol": "CRC"
+}, {
+    "name": "Croatia Kuna",
+    "symbol": "HRK"
+}, {
+    "name": "Cuba Peso",
+    "symbol": "CUP"
+}, {
+    "name": "Czech Republic Koruna",
+    "symbol": "CZK"
+}, {
+    "name": "Denmark Krone",
+    "symbol": "DKK"
+}, {
+    "name": "Djibouti Franc",
+    "symbol": "DJF"
+}, {
+    "name": "Dominican Republic Peso",
+    "symbol": "DOP"
+}, {
+    "name": "East Caribbean Dollar",
+    "symbol": "XCD"
+}, {
+    "name": "Egypt Pound",
+    "symbol": "EGP"
+}, {
+    "name": "El Salvador Colon",
+    "symbol": "SVC"
+}, {
+    "name": "Ethiopia Birr",
+    "symbol": "ETB"
+}, {
+    "name": "Euro Member Countries",
+    "symbol": "EUR"
+}, {
+    "name": "Fiji Dollar",
+    "symbol": "FJD"
+}, {
+    "name": "Gambia Dalasi",
+    "symbol": "GMD"
+}, {
+    "name": "Georgia Lari",
+    "symbol": "GEL"
+}, {
+    "name": "Ghana Cedi",
+    "symbol": "GHS"
+}, {
+    "name": "Guatemala Quetzal",
+    "symbol": "GTQ"
+}, {
+    "name": "Guinea Franc",
+    "symbol": "GNF"
+}, {
+    "name": "Guyana Dollar",
+    "symbol": "GYD"
+}, {
+    "name": "Haiti Gourde",
+    "symbol": "HTG"
+}, {
+    "name": "Honduras Lempira",
+    "symbol": "HNL"
+}, {
+    "name": "Hong Kong Dollar",
+    "symbol": "HKD"
+}, {
+    "name": "Hungary Forint",
+    "symbol": "HUF"
+}, {
+    "name": "Iceland Krona",
+    "symbol": "ISK"
+}, {
+    "name": "India Rupee",
+    "symbol": "INR"
+}, {
+    "name": "Indonesia Rupiah",
+    "symbol": "IDR"
+}, {
+    "name": "Iran Rial",
+    "symbol": "IRR"
+}, {
+    "name": "Iraq Dinar",
+    "symbol": "IQD"
+}, {
+    "name": "Israel Shekel",
+    "symbol": "ILS"
+}, {
+    "name": "Jamaica Dollar",
+    "symbol": "JMD"
+}, {
+    "name": "Japan Yen",
+    "symbol": "JPY"
+}, {
+    "name": "Jordan Dinar",
+    "symbol": "JOD"
+}, {
+    "name": "Kazakhstan Tenge",
+    "symbol": "KZT"
+}, {
+    "name": "Kenya Shilling",
+    "symbol": "KES"
+}, {
+    "name": "Korea (South) Won",
+    "symbol": "KRW"
+}, {
+    "name": "Kuwait Dinar",
+    "symbol": "KWD"
+}, {
+    "name": "Kyrgyzstan Som",
+    "symbol": "KGS"
+}, {
+    "name": "Laos Kip",
+    "symbol": "LAK"
+}, {
+    "name": "Lebanon Pound",
+    "symbol": "LBP"
+}, {
+    "name": "Lesotho Loti",
+    "symbol": "LSL"
+}, {
+    "name": "Liberia Dollar",
+    "symbol": "LRD"
+}, {
+    "name": "Libya Dinar",
+    "symbol": "LYD"
+}, {
+    "name": "Macau Pataca",
+    "symbol": "MOP"
+}, {
+    "name": "Macedonia Denar",
+    "symbol": "MKD"
+}, {
+    "name": "Madagascar Ariary",
+    "symbol": "MGA"
+}, {
+    "name": "Malawi Kwacha",
+    "symbol": "MWK"
+}, {
+    "name": "Malaysia Ringgit",
+    "symbol": "MYR"
+}, {
+    "name": "Maldives (Maldive Islands) Rufiyaa",
+    "symbol": "MVR"
+}, {
+    "name": "Mauritius Rupee",
+    "symbol": "MUR"
+}, {
+    "name": "Mexico Peso",
+    "symbol": "MXN"
+}, {
+    "name": "Moldova Leu",
+    "symbol": "MDL"
+}, {
+    "name": "Morocco Dirham",
+    "symbol": "MAD"
+}, {
+    "name": "Mozambique Metical",
+    "symbol": "MZN"
+}, {
+    "name": "Myanmar (Burma) Kyat",
+    "symbol": "MMK"
+}, {
+    "name": "Namibia Dollar",
+    "symbol": "NAD"
+}, {
+    "name": "Nepal Rupee",
+    "symbol": "NPR"
+}, {
+    "name": "Netherlands Antilles Guilder",
+    "symbol": "ANG"
+}, {
+    "name": "New Zealand Dollar",
+    "symbol": "NZD"
+}, {
+    "name": "Nicaragua Cordoba",
+    "symbol": "NIO"
+}, {
+    "name": "Nigeria Naira",
+    "symbol": "NGN"
+}, {
+    "name": "Norway Krone",
+    "symbol": "NOK"
+}, {
+    "name": "Oman Rial",
+    "symbol": "OMR"
+}, {
+    "name": "Pakistan Rupee",
+    "symbol": "PKR"
+}, {
+    "name": "Panama Balboa",
+    "symbol": "PAB"
+}, {
+    "name": "Papua New Guinea Kina",
+    "symbol": "PGK"
+}, {
+    "name": "Paraguay Guarani",
+    "symbol": "PYG"
+}, {
+    "name": "Peru Sol",
+    "symbol": "PEN"
+}, {
+    "name": "Philippines Peso",
+    "symbol": "PHP"
+}, {
+    "name": "Poland Zloty",
+    "symbol": "PLN"
+}, {
+    "name": "Qatar Riyal",
+    "symbol": "QAR"
+}, {
+    "name": "Romania Leu",
+    "symbol": "RON"
+}, {
+    "name": "Russia Ruble",
+    "symbol": "RUB"
+}, {
+    "name": "Rwanda Franc",
+    "symbol": "RWF"
+}, {
+    "name": "Saudi Arabia Riyal",
+    "symbol": "SAR"
+}, {
+    "name": "Serbia Dinar",
+    "symbol": "RSD"
+}, {
+    "name": "Seychelles Rupee",
+    "symbol": "SCR"
+}, {
+    "name": "Sierra Leone Leone",
+    "symbol": "SLL"
+}, {
+    "name": "Singapore Dollar",
+    "symbol": "SGD"
+}, {
+    "name": "Solomon Islands Dollar",
+    "symbol": "SBD"
+}, {
+    "name": "Somalia Shilling",
+    "symbol": "SOS"
+}, {
+    "name": "South Africa Rand",
+    "symbol": "ZAR"
+}, {
+    "name": "Sri Lanka Rupee",
+    "symbol": "LKR"
+}, {
+    "name": "Sudan Pound",
+    "symbol": "SDG"
+}, {
+    "name": "Suriname Dollar",
+    "symbol": "SRD"
+}, {
+    "name": "Swaziland Lilangeni",
+    "symbol": "SZL"
+}, {
+    "name": "Sweden Krona",
+    "symbol": "SEK"
+}, {
+    "name": "Switzerland Franc",
+    "symbol": "CHF"
+}, {
+    "name": "Taiwan New Dollar",
+    "symbol": "TWD"
+}, {
+    "name": "Tajikistan Somoni",
+    "symbol": "TJS"
+}, {
+    "name": "Tanzania Shilling",
+    "symbol": "TZS"
+}, {
+    "name": "Thailand Baht",
+    "symbol": "THB"
+}, {
+    "name": "Tonga Pa'anga",
+    "symbol": "TOP"
+}, {
+    "name": "Trinidad and Tobago Dollar",
+    "symbol": "TTD"
+}, {
+    "name": "Tunisia Dinar",
+    "symbol": "TND"
+}, {
+    "name": "Turkey Lira",
+    "symbol": "TRY"
+}, {
+    "name": "Turkmenistan Manat",
+    "symbol": "TMT"
+}, {
+    "name": "Uganda Shilling",
+    "symbol": "UGX"
+}, {
+    "name": "Ukraine Hryvnia",
+    "symbol": "UAH"
+}, {
+    "name": "United Arab Emirates Dirham",
+    "symbol": "AED"
+}, {
+    "name": "United Kingdom Pound",
+    "symbol": "GBP"
+}, {
+    "name": "Uruguay Peso",
+    "symbol": "UYU"
+}, {
+    "name": "Uzbekistan Som",
+    "symbol": "UZS"
+}, {
+    "name": "Viet Nam Dong",
+    "symbol": "VND"
+}, {
+    "name": "Yemen Rial",
+    "symbol": "YER"
+}, {
+    "name": "Zambia Kwacha",
+    "symbol": "ZMW"
+}, {
+    "name": "Afghan Afghani",
+    "symbol": "AFN"
+}, {
+    "name": "Unidad de Fomento",
+    "symbol": "CLF"
+}, {
+    "name": "Chinese Renminbi Yuan Offshore",
+    "symbol": "CNH"
+}, {
+    "name": "Ethereum",
+    "symbol": "ETH"
+}, {
+    "name": "Litecoin",
+    "symbol": "LTC"
+}, {
+    "name": "Belarusian Ruble",
+    "symbol": "BYR"
+}, {
+    "name": "Cuban Convertible Peso",
+    "symbol": "CUC"
+}, {
+    "name": "Estonian Kroon",
+    "symbol": "EEK"
+}, {
+    "name": "Eritrean Nakfa",
+    "symbol": "ERN"
+}, {
+    "name": "Falkland Pound",
+    "symbol": "FKP"
+}, {
+    "name": "Guernsey Pound",
+    "symbol": "GGP"
+}, {
+    "name": "Gibraltar Pound",
+    "symbol": "GIP"
+}, {
+    "name": "Isle of Man Pound",
+    "symbol": "IMP"
+}, {
+    "name": "Jersey Pound",
+    "symbol": "JEP"
+}, {
+    "name": "Lithuanian Litas",
+    "symbol": "LTL"
+}, {
+    "name": "Maltese Lira",
+    "symbol": "MTL"
+}, {
+    "name": "Saint Helenian Pound",
+    "symbol": "SHP"
+}, {
+    "name": "São Tomé and Príncipe Dobra",
+    "symbol": "STD"
+}, {
+    "name": "East Caribbean Dollar",
+    "symbol": "XCD"
+}];
